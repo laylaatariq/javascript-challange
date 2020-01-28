c589e8eaacc519448204faaacd5bf17d8e9d554c
Laylaa Tariq <laaylatariq@gmail.com>
2020-01-27 19:10:52 -0600

    done with level 2 as well

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -20,16 +20,38 @@ var button = d3.select("#filter");
 
 button.on("click", function() {
 
+	//Get which category is choosen by the user
+	var categoryChoice = d3.select(".select");
+
 	// Select the input element and get the raw HTML node
 	var inputElement = d3.select("#search");
 
 	// Get the value property of the input element
 	var inputValue = inputElement.property("value");
 
-	console.log(inputValue);
-	console.log(tableData);
+	//Get the property value for the choice
+	var choice = categoryChoice.property("value");
 
-	var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+	console.log(inputValue);
+	console.log(choice);
+
+	var filteredData = {};
+
+	if(choice === "date"){
+		filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+	}
+	else if(choice === "city"){
+		filteredData = tableData.filter(tableData => tableData.city === inputValue);
+	}
+	else if(choice === "state"){
+		filteredData = tableData.filter(tableData => tableData.state=== inputValue);
+	}
+	else if(choice === "country"){
+		filteredData = tableData.filter(tableData => tableData.country === inputValue);
+	}
+	else{
+		filteredData = tableData.filter(tableData => tableData.shape === inputValue);
+	}
 
 	console.log(filteredData);
 
